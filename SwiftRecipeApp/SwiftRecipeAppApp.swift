0cec466589e67b57b55974fd2a3a6ff85e8a5991
Eason Lin <lin00212@algonquinlive.com>
2024-10-30 16:10:24 -0400

    Initial Commit

---
+++ SwiftRecipeApp/SwiftRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRecipeAppApp.swift
+//  SwiftRecipeApp
+//
+//  Created by Eason Lin on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
